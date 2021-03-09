d0dd33d19b26258e206a09eb16372ef0ea417d9c
facu2279 <2279@holbertonschool.com>
2021-03-09 14:19:29 +0000

    Add task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-
+/**
+* binary_tree_node - create a node
+* @parent: pointer to parent
+* @value: value of new node
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
